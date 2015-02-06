9845e1a555982669b29924aafb4e19d27b40ebca
drgef <thedrgef@gmail.com>
2015-02-06 21:20:56 +0300

    prepare dev and des blocks

--- js/script.js
+++ js/script.js
@@ -11,6 +11,22 @@ var main = function() {
 	setTimeout(function(){
 		closeMenu();
 	}, 1000);
+	// плавная прокрутка begin
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 300);
+	        return false;
+	      }
+	    }
+	  });
+	});
+	// плавная прокрутка end
 }
 
 $(document).ready(main);
